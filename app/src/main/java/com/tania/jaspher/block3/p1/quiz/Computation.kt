23e71c5d9c46634974f445d2aff0e53b2b2a1290
Jaspher Tania <taniajaspher@gmail.com>
2023-12-07 23:49:50 +0800

    second commit

--- app/src/main/java/com/tania/jaspher/block3/p1/quiz/Computation.kt
+++ app/src/main/java/com/tania/jaspher/block3/p1/quiz/Computation.kt
@@ -2,10 +2,42 @@ package com.tania.jaspher.block3.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class Computation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_computation)
+
+        val editTextWeight: EditText = findViewById(R.id.editTextWeight)
+        val editTextHeight: EditText = findViewById(R.id.editTextHeight)
+        val btnCalculate: Button = findViewById(R.id.btnCalculate)
+        val textViewResult: TextView = findViewById(R.id.textViewResult)
+
+        btnCalculate.setOnClickListener{
+            calculateBMI(editTextWeight.text.toString(), editTextHeight.text.toString(), textViewResult)
+        }
+    }
+
+    private fun calculateBMI(weight: String, height: String, resultTextView: TextView) {
+        if(weight.isNotEmpty() && height.isNotEmpty()) {
+            val weightValue = weight.toFloat()
+            val heightValue = height.toFloat() / 100
+            val bmi = weightValue / (heightValue * weightValue)
+
+            val result = when {
+                bmi < 18.5 -> "You are Underweight"
+                bmi < 24.9 -> "You have a Normal Weight"
+                bmi < 29.9 -> "You are Overweight"
+                else -> "You are Obese"
+
+            }
+
+            resultTextView.text = "BMI: ${String.format("%.2f", bmi)}\nCategory: $result"
+        } else {
+            resultTextView.text = "Enter a valid height and weight."
+        }
     }
 }
--- app/src/main/java/com/tania/jaspher/block3/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/tania/jaspher/block3/p1/quiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tania.jaspher.block3.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,13 +9,14 @@ import android.widget.GridLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val startButton: Button = findViewById(R.id.bt_start)
+        startButton.setOnClickListener {startComputationActivity()}
 
-
+        //Exit button
         val exitButton: Button = findViewById(R.id.bt_exit)
         exitButton.setOnClickListener { exitApp(it) }
 
@@ -23,4 +25,10 @@ class MainActivity : AppCompatActivity() {
         fun exitApp(view: View){
             finish()
         }
+
+    private fun startComputationActivity() {
+        val intent = Intent(this, Computation:: class.java)
+        startActivity(intent)
+    }
+
 }
